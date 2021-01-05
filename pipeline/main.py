94caaef3c2fe29292dcbffa59d8f6d734c01227f
= <=>
2021-01-06 00:34:53 +0700

    Upload code for final project

---
+++ pipeline/main.py
@@ -0,0 +1,15 @@
+from pickle import load
+from numpy import array
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.utils.vis_utils import plot_model
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Embedding
+from keras.layers import RepeatVector
+from keras.layers import TimeDistributed
+from keras.callbacks import ModelCheckpoint
+
+
---
+++ vectorization/encode_decode.py
@@ -0,0 +1,58 @@
+
+from pickle import load
+from pickle import dump
+from numpy.random import rand
+from numpy.random import shuffle
+
+# load a clean dataset
+def load_clean_sentences(filename):
+	return load(open(filename, 'rb'))
+
+# fit a tokenizer
+def create_tokenizer(lines):
+	tokenizer = Tokenizer()
+	tokenizer.fit_on_texts(lines)
+	return tokenizer
+
+# max sentence length
+def max_length(lines):
+	return max(len(line.split()) for line in lines)
+
+# encode and pad sequences
+def encode_sequences(tokenizer, length, lines):
+	# integer encode sequences
+	X = tokenizer.texts_to_sequences(lines)
+	# pad sequences with 0 values
+	X = pad_sequences(X, maxlen=length, padding='post')
+	return X
+
+
+# one hot encode target sequence
+def encode_output(sequences, vocab_size):
+	ylist = list()
+	for sequence in sequences:
+		encoded = to_categorical(sequence, num_classes=vocab_size)
+		ylist.append(encoded)
+	y = array(ylist)
+	y = y.reshape(sequences.shape[0], sequences.shape[1], vocab_size)
+	return y
+
+def main():
+    # prepare english tokenizer
+    eng_tokenizer = create_tokenizer(dataset[:, 0])
+    eng_vocab_size = len(eng_tokenizer.word_index) + 1
+    eng_length = max_length(dataset[:, 0])
+    print('English Vocabulary Size: %d' % eng_vocab_size)
+    print('English Max Length: %d' % (eng_length))
+    print("English tokenize: ", eng_tokenizer )
+    # prepare vietnameses tokenizer
+    vi_tokenizer = create_tokenizer(dataset[:, 1])
+    vi_vocab_size = len(vi_tokenizer.word_index) + 1
+    vi_length = max_length(dataset[:, 1])
+    print('Vietnamese Vocabulary Size: %d' % vi_vocab_size)
+    print('Vietnamese Max Length: %d' % (vi_length))
+    print('Vietnamese tokenize: ', vi_tokenizer)
+
+
+if __name__ == "__main__":
+    pass 
