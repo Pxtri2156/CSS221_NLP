3f065827fa5d1a7a61bc8571899fb42fb7de2dbe
= <=>
2021-01-06 08:19:37 +0700

    Update pipeline

---
+++ pipeline/model.py
@@ -0,0 +1,47 @@
+from pickle import load
+from numpy import array
+import argparse
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.utils.vis_utils import plot_model
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Embedding
+from keras.layers import RepeatVector
+from keras.layers import TimeDistributed
+from keras.callbacks import ModelCheckpoint
+
+    # define NMT model
+def define_model(src_vocab, tar_vocab, src_timesteps, tar_timesteps, n_units):
+	model = Sequential()
+	model.add(Embedding(src_vocab, n_units, input_length=src_timesteps, mask_zero=True))
+	model.add(LSTM(n_units))
+	model.add(RepeatVector(tar_timesteps))
+	model.add(LSTM(n_units, return_sequences=True))
+	model.add(TimeDistributed(Dense(tar_vocab, activation='softmax')))
+	return model
+
+def main(args):
+    # # define model
+    # model = define_model(eng_vocab_size, vi_vocab_size, eng_length, vi_length, 256)
+    # model.compile(optimizer='adam', loss='categorical_crossentropy')
+    # # summarize defined model
+    # print(model.summary())
+    # plot_model(model, to_file='model.png', show_shapes=True)
+
+    # fit model
+    print("model_path: ", filename)
+    input()
+    checkpoint = ModelCheckpoint(filename, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+    model.fit(trainX, trainY, epochs=30, batch_size=64, validation_data=(testX, testY), callbacks=[checkpoint], verbose=2)
+
+def args_parse():
+    parser = argparse.ArgumentParser(description="Nature language processing ")
+    parser.add_argument('-m', '--model_path',  default="./model.h5",
+                        help="The path to save model ")
+    return parser.parse_args()
+if __name__ == "__main__":
+    args = args_parse()
+    main(args)
